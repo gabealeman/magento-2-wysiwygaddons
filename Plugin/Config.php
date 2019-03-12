c15c9423d03cb222c131acb4ffe053e33285e2fa
Gabe Aleman <gabealeman@Gabes-MBP.attlocal.net>
2019-03-12 13:15:07 -0500

    inital commit

---
+++ Plugin/Config.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace gabealeman\WysiwygAddons\Plugin;
+
+
+class Config
+{
+
+    protected $activeEditor;
+
+    public function __construct(\Magento\Ui\Block\Wysiwyg\ActiveEditor $activeEditor)
+    {
+        $this->activeEditor = $activeEditor;
+    }
+
+    /**
+     * Return WYSIWYG configuration
+     *
+     * @param \Magento\Ui\Component\Wysiwyg\ConfigInterface $configInterface
+     * @param \Magento\Framework\DataObject $result
+     * @return \Magento\Framework\DataObject
+     */
+    public function afterGetConfig(
+        \Magento\Ui\Component\Wysiwyg\ConfigInterface $configInterface,
+        \Magento\Framework\DataObject $result
+    ) {
+
+        $editor = $this->activeEditor->getWysiwygAdapterPath();
+
+        if(strpos($editor,'tinymce4Adapter')){
+
+            if (($result->getDataByPath('settings/menubar')) || ($result->getDataByPath('settings/toolbar')) || ($result->getDataByPath('settings/plugins'))){
+                return $result;
+            }
+
+            $settings = $result->getData('settings');
+
+            if (!is_array($settings)) {
+                $settings = [];
+            }
+
+            $settings['menubar'] = true;
+            $settings['toolbar'] = 'undo redo | styleselect | fontsizeselect | link | forecolor backcolor | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | table | image | code';
+            $settings['plugins'] = 'textcolor image code visualblocks link';
+            $settings['visualblocks_default_state'] =  true;
+
+            $settings['extended_valid_elements'] = 'svg[*],defs[*],pattern[*],desc[*],metadata[*],g[*],mask[*],path[*],line[*],marker[*],rect[*],circle[*],ellipse[*],polygon[*],polyline[*],linearGradient[*],radialGradient[*],stop[*],image[*],view[*],text[*],textPath[*],title[*],tspan[*],glyph[*],symbol[*],switch[*],use[*]';
+            $result->setData('settings', $settings);
+            return $result;
+        }
+        else{
+            return $result;
+        }
+    }
+}
---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'gabealeman_WysiwygAddons',
+    __DIR__
+);
